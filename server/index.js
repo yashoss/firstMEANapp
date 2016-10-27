18c23d9336ca058621edcd2f630feffa0fcd2b8b
yasin <yashoss@gmail.com>
2016-10-27 14:59:10 -0700

    Added CORS support

--- server/index.js
+++ server/index.js
@@ -11,3 +11,18 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodeOverride('X-HTTP-Method-Override'));
+
+//CORS support
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/first_MEAN_app');
+mongoose.connection.once('open', function(){
+  console.log('Listening on port 3000...');
+  app.listen(3000);
+});
